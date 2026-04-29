df05827f10946e1ea36ae134da0971d759a813ab
Ewa Zalewska <L00196864@atu.ie>
2026-04-29 14:23:59 +0100

    Implement SystemViewModel and bind system list to UI

---
+++ ViewModels/SystemViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using CSVGxpInventoryApp.Models;
+using CSVGxpInventoryApp.Repositories;
+
+namespace CSVGxpInventoryApp.ViewModels;
+
+public class SystemViewModel
+{
+    private readonly SystemRepository _systemRepository;
+
+    public ObservableCollection<SystemEntity> Systems { get; set; } = new();
+
+    public SystemViewModel(SystemRepository systemRepository)
+    {
+        _systemRepository = systemRepository;
+    }
+
+    public async Task LoadSystemsAsync()
+    {
+        var systems = await _systemRepository.GetSystemsAsync();
+
+        Systems.Clear();
+
+        foreach (var system in systems)
+        {
+            Systems.Add(system);
+        }
+    }
+}
